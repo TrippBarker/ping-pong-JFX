cb7b320418c79de16a16afbc4f9443bdfa9e0826
Tripp Barker <colemanbarker@gmail.com>
2023-03-16 23:52:31 -0400

    working on button builder

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,11 @@ import application.controllers.StartController;
 import application.scenes.StartScene;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
@@ -16,9 +19,9 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage startStage) throws Exception {
-		BorderPane root = new BorderPane();
+		Group root = new Group();
 		StartScene startScene = new StartScene();
-		Scene scene = startScene.buildScene(gridLenUnit, sceneWidth, sceneHeight, root);
+		Scene scene = startScene.buildScene(gridLenUnit, sceneHeight, sceneWidth, root);
 		startStage.setTitle("PING PONG");
 		startStage.setResizable(false);
 		startStage.setScene(scene);
--- src/application/scenes/StartScene.java
+++ src/application/scenes/StartScene.java
@@ -1,12 +1,27 @@
 package application.scenes;
 
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 public class StartScene {
 	
-	public Scene buildScene(int gridLenUnit, int sceneWidth, int sceneHeight, BorderPane root) {
+	public Scene buildScene(int gridLenUnit, int sceneHeight, int sceneWidth, Group root) {
+		root.getChildren().add(createButton("START", sceneWidth / 2, sceneHeight / 2));
+		root.getChildren().add(createButton("EXIT", sceneWidth / 2, (sceneHeight / 4) * 3));
 		Scene startScene = new Scene(root, sceneWidth, sceneHeight);
 		return startScene;
+	}
+	
+	private Button createButton(String text, int x, int y) {
+		Button button = new Button(text);
+		button.setPrefWidth(100);
+		button.setPrefHeight(50);
+		button.setTranslateX(x - 50);
+		button.setTranslateY(y - 50);
+		return button;
 	}
 }
