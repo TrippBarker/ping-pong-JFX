7dd57df99819635d7fbe89e51261e805fe9b4100
Tripp Barker <colemanbarker@gmail.com>
2023-03-17 21:02:20 -0400

    added key event listener

--- src/application/controllers/PlayController.java
+++ src/application/controllers/PlayController.java
@@ -1,7 +1,9 @@
 package application.controllers;
 
 import application.scenes.SceneSwitcher;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class PlayController {
@@ -12,5 +14,17 @@ public class PlayController {
 	public PlayController(Scene scene, Stage stage) {
 		this.scene = scene;
 		this.stage = stage;
+		addListeners();
+	}
+	
+	public void addListeners() {
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent ke) {
+				System.out.println(ke.getCode());
+			}
+			
+		});
 	}
 }
