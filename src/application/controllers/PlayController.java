7b5d7a49c2c7047b99ce11dd856a06d454ebd78c
Tripp Barker <colemanbarker@gmail.com>
2023-03-18 12:07:49 -0400

    working on paddle movements

--- src/application/controllers/PlayController.java
+++ src/application/controllers/PlayController.java
@@ -1,15 +1,21 @@
 package application.controllers;
 
 import application.scenes.SceneSwitcher;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class PlayController {
 	private Scene scene;
 	private Stage stage;
 	private SceneSwitcher ss;
+	private int padSpeed = 1;
+	private String[] actions = {"", "", ""};
+
 	
 	public PlayController(Scene scene, Stage stage) {
 		this.scene = scene;
@@ -18,13 +24,66 @@ public class PlayController {
 	}
 	
 	public void addListeners() {
-		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-
-			@Override
-			public void handle(KeyEvent ke) {
-				System.out.println(ke.getCode());
-			}
-			
-		});
+		ObservableList nodes = scene.getRoot().getChildrenUnmodifiable();
+		Rectangle leftPad = (Rectangle) nodes.get(0);
+		Rectangle rightPad = (Rectangle) nodes.get(1);
+			scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent ke) {
+					String key = ke.getCode().toString();
+					System.out.println(ke.getEventType());
+					switch (key) {
+					case "W":
+						leftPad.setTranslateY(leftPad.getTranslateY() - padSpeed);
+						actions[0] = "up";
+						break;
+					case "S":
+						leftPad.setTranslateY(leftPad.getTranslateY() + padSpeed);
+						actions[0] = "down";
+						break;
+					case "UP":
+						rightPad.setTranslateY(rightPad.getTranslateY() - padSpeed);
+						actions[1] = "up";
+						break;
+					case "DOWN":
+						rightPad.setTranslateY(rightPad.getTranslateY() + padSpeed);
+						actions[1] = "down";
+						break;
+					case "SPACE":
+						actions[2] = "fire";
+						break;
+					}
+				}
+				
+			});
+			scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent ke) {
+					String key = ke.getCode().toString();
+					System.out.println(ke.getEventType());
+					switch (key) {
+					case "W":
+						leftPad.setTranslateY(leftPad.getTranslateY() - padSpeed);
+						actions[0] = "";
+						break;
+					case "S":
+						leftPad.setTranslateY(leftPad.getTranslateY() + padSpeed);
+						actions[0] = "";
+						break;
+					case "UP":
+						rightPad.setTranslateY(rightPad.getTranslateY() - padSpeed);
+						actions[1] = "";
+						break;
+					case "DOWN":
+						rightPad.setTranslateY(rightPad.getTranslateY() + padSpeed);
+						actions[1] = "";
+						break;
+					case "SPACE":
+						actions[2] = "";
+						break;
+					}
+				}
+			});
+		
 	}
 }
